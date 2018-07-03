67d611ff79f151d28f32f97ab8e9f2dc2caea1cc
Andrew Hartford <andrewhartford@utexas.edu>
2018-07-03 13:37:51 -0500

    Daily update

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -6,7 +6,26 @@ import './Main.css';
 function Main() {
     return (
         <div>
-            <h1 id="welcome">Andrew Hartford </h1>
+            <h1 id="welcome">Andrew Hartford</h1>
+
+
+
+            <div class="container">
+    <div class="row">
+      <div class="col-sm">
+
+        <img src="https://www.usnews.com/img/college-photo_6539_256x256mm.jpg"/>
+        <img src="https://www.usnews.com/img/college-photo_6539_256x256mm.jpg"/>
+      </div>
+      </div>
+      </div>
+
+
+
+
+
+
+
             <CountriesContainer />
             <CreateCountriesContainer />
         </div>
