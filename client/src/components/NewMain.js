6721df5f3bbf7a2d71fee2cc624206a5752dfe83
Andrew Hartford <andrewhartford@utexas.edu>
2018-08-13 22:05:14 -0500

    new update2

--- client/src/components/NewMain.js
+++ client/src/components/NewMain.js
@@ -4,6 +4,8 @@ import './Main.css';
 function NewMain() {
     return (
         <div>
+            
+         <div class="text-pop-up-top"><p>andrew</p></div>
             <h1 className="inset-text">Andrew Hartford</h1>
             <p1 className="slide-top">Full Stack Developer in Austin, TX</p1>
         </div>
