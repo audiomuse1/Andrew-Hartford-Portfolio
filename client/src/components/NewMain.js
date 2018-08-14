3df57f731349002db23a1249eda8a38c932a9cf3
Andrew Hartford <andrewhartford@utexas.edu>
2018-08-13 22:19:15 -0500

    added to nav bar

--- client/src/components/NewMain.js
+++ client/src/components/NewMain.js
@@ -5,7 +5,6 @@ function NewMain() {
     return (
         <div>
             
-         <div class="text-pop-up-top"><p>andrew</p></div>
             <h1 className="inset-text">Andrew Hartford</h1>
             <p1 className="slide-top">Full Stack Developer in Austin, TX</p1>
         </div>
