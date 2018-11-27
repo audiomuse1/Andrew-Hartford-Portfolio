27170577a1620e5c045cb4af379714d084583ba1
Andrew Hartford <andrewhartford@utexas.edu>
2018-11-26 22:09:05 -0600

    todays update

--- client/src/components/NewMain.js
+++ client/src/components/NewMain.js
@@ -10,7 +10,7 @@ function NewMain() {
             
             <h1 className="inset-text text-focus-in">Bumperactive.com</h1>
             
-            <p1 className="text-focus-in">Custom Stickers and Decals of All Kinds Made Us Famous...</p1>
+            <p1 className="text-focus-in">Custom Stickers and Decals of All Kinds Made Us Famous.</p1>
 
 
         </div>
