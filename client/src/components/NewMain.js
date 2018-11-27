136eac446cebbb2f0e097f485910a21de3e102fb
Andrew Work <andrewhartford@utexas.edu>
2018-11-27 17:01:18 -0600

    new

--- client/src/components/NewMain.js
+++ client/src/components/NewMain.js
@@ -4,7 +4,7 @@ import './Main.css';
 
 function NewMain() {
 
-            
+            <div>
             <h1 className="inset-text text-focus-in">Bumperactive.com</h1>
             
             <p1 className="text-focus-in">Custom Stickers and Decals of All Kinds Made Us Famous.</p1>
