1f8523af19b89b1a83ce904b2d98042ee2642549
Andrew Hartford <andrewhartford@utexas.edu>
2018-12-02 23:04:56 -0600

    new heading

--- client/src/components/NewMain.js
+++ client/src/components/NewMain.js
@@ -7,7 +7,7 @@ function NewMain() {
             <div>
             <h1 className="inset-text text-focus-in">Bumperactive.com</h1>
             
-            <p1 className="text-focus-in">Custom Stickers and Decals of All Kinds Made Us Famous.</p1>
+            <p1 className="text-focus-in">Custom Stickers and Decals of All Kinds Made Us Famous</p1>
 
             {/* <div className="yee">
             <p1 className="hello text-focus-in">Custom Stickers and Decals of All Kinds Made Us Famous...</p1>
